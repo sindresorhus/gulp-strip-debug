d2afa92fa65ec5ac8c4a79619252929538ca7c5d
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-19 18:44:23 +0100

    switch to through2

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 'use strict';
 var gutil = require('gulp-util');
-var through = require('through');
+var through = require('through2');
 var stripDebug = require('strip-debug');
 
 module.exports = function () {
-	return through(function (file) {
+	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
-			return this.queue(file);
+			this.push(file);
+			return cb();
 		}
 
 		if (file.isStream()) {
-			return this.emit('error', new gutil.PluginError('gulp-strip-debug', 'Streaming not supported'));
+			this.emit('error', new gutil.PluginError('gulp-strip-debug', 'Streaming not supported'));
+			return cb();
 		}
 
 		try {
 			file.contents = new Buffer(stripDebug(file.contents.toString()).toString());
 		} catch (err) {
-			return this.emit('error', new gutil.PluginError('gulp-strip-debug', err));
+			this.emit('error', new gutil.PluginError('gulp-strip-debug', err));
 		}
 
-		this.queue(file);
+		this.push(file);
+		cb();
 	});
 };
